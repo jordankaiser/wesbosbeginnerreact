4e73277246999ba2d211ed4631a1acf1ef8930a1
Jordan Kaiser <jkmacbook@Jordans-MacBook-Pro.local>
2020-02-26 16:02:06 -0600

    finished 15

---
+++ catch-of-the-day/src/components/Fish.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import { formatPrice } from '../helpers'
+
+class Fish extends React.Component {
+  render() {
+    const image = this.props.details.image
+    const name = this.props.details.name
+    const price = this.props.details.price
+    const desc = this.props.details.desc
+    const status = this.props.details.status
+    return (
+      <li className="menu-fish">
+        <img src={image} alt={name} />
+        <h3 className="fish-name">
+          {name}
+          <span>{formatPrice(price)}</span>
+        </h3>
+        <p>{desc}</p>
+        <button>Add To Cart</button>
+      </li>
+    )
+  }
+}
+
+export default Fish
--- catch-of-the-day/src/helpers/helpers.js
+++ catch-of-the-day/src/helpers/helpers.js
@@ -1,3 +1,6 @@
-export function helper() {
-  console.log('a helper function')
+export function formatPrice(cents) {
+  return (cents / 100).toLocaleString("en-US", {
+    style: "currency",
+    currency: "USD"
+  });
 }
