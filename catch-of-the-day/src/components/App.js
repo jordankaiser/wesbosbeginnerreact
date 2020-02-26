107a86f29586007e80a204a6a8d613a630122e9e
Jordan Kaiser <jkmacbook@Jordans-MacBook-Pro.local>
2020-02-26 14:50:52 -0600

    Progress

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -4,14 +4,21 @@ import Inventory from './Inventory'
 import Order from './Order'
 
 class App extends React.Component {
+  state = {
+    fishes: {},
+    order: {},
+  }
+  addFish = (fish) => {
+    console.log('adding a fish')
+  }
   render() {
     return (
       <div className="catch-of-the-day">
         <div className="menu">
           <Header tagline='Fresh Seafood Market' />
         </div>
-        <Inventory />
         <Order />
+        <Inventory addFish={this.addFish} />
       </div>
     )
   }
